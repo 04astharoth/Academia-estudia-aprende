4f2842b8658975172709ff50fe934b49045a17a3
Jose Gil Varon Moreno <me@example.com>
2021-06-17 20:17:26 -0500

    creacion peticion ajax, guardado del JSON, Obtencion y ordenamiento de eventos

--- js/index.js
+++ js/index.js
@@ -1,24 +1,51 @@
 // Hemos omitido los acentos en los comentarios por compatibilidad
 
-//Define las variables que necesites
-
-
 $(document).ready(function () {
+  // Funcion para ordenar los eventos
+  function organizarEventos(eventos,fechaReferencia) {
+    //Clasifica los eventos segun la fecha actual del JSON
+    var e_pasados = eventos.filter(e => new Date(e.fecha).getTime() <= new Date(fechaReferencia).getTime());
+    var e_futuros = eventos.filter(e => new Date(e.fecha).getTime() >= new Date(fechaReferencia).getTime());
+    //Ordena los eventos segun la fecha (los mas cercanos primero)
+    e_pasados = e_pasados.sort((a,b) => {
+      if (new Date(a.fecha).getTime() > new Date(b.fecha).getTime()){
+        return -1; // devuelve -1 para ordenar del mas reciente al mas antiguo
+      }
+      if (new Date(a.fecha).getTime() < new Date(b.fecha).getTime()){
+        return 1;
+      }
+      return 0;
+    });
+    
+    e_futuros = e_futuros.sort((a,b) => {
+      if (new Date(a.fecha).getTime() > new Date(b.fecha).getTime()){
+        return 1; // devuelve 1 para ordenar del mas reciente al mas lejano
+      }
+      if (new Date(a.fecha).getTime() < new Date(b.fecha).getTime()){
+        return -1;
+      }
+      return 0;
+    });
+    
+    return [e_futuros,e_pasados];
+  }
+//Define las variables que necesites
+var eventos, resultado, fechaActual, eventosPasados, eventosProximos;
 
   //Carga los datos que estan en el JSON (info.json) usando AJAX
-
-  //Guarda el resultado en variables
-
-  //Clasifica los eventos segun la fecha actual del JSON
-
-  //Ordena los eventos segun la fecha (los mas cercanos primero)
-
-  //Extrae solo dos eventos
-
-  //Ordena los eventos segun la fecha (los mas cercanos primero)
-
-  //Extrae solo dos eventos
-
+  $.ajax({
+    url:"http://127.0.0.1:5500/info.json"
+  }).done(function (peticion) {
+    //Guarda el resultado en variables
+    eventos = peticion.eventos;
+    fechaActual = peticion.fechaActual;
+    //obteniendo los eventos clasificados y ordenados
+    resultado = organizarEventos(eventos, new Date(fechaActual));
+    //Extrae solo dos eventos de cada tipo
+    eventosPasados = [resultado[1][0],resultado[1][1]];
+    eventosProximos = [resultado[0][0],resultado[0][1]];
+  });
+  
   //Crea un string que contenga el HTML que describe el detalle del evento
 
   //Recorre el arreglo y concatena el HTML para cada evento

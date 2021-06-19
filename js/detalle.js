c0cd6acd3e66bc59922abbcd8eb80da140a7196e
Jose Gil Varon Moreno <me@example.com>
2021-06-19 17:30:24 -0500

    construccion de la vista detalle de los eventos

--- js/detalle.js
+++ js/detalle.js
@@ -1,17 +1,33 @@
-// Hemos omitido los acentos en los comentarios por compatibilidad
-
 $(document).ready(function () {
 
-  //Esta es la instruccion para tomar el id del URL detalle.html?id=<identificador>
+  //declaracion de variables
+  var eventos;
 
-  //Carga los datos que estan en el JSON (info.json) usando AJAX
+  $.ajax({
+    url: "http://127.0.0.1:5500/info.json"
+  }).done(function (peticion) {
+    //Guarda el resultado en una variable
+    eventos = peticion.eventos;
+    console.log(peticion.xhr);
+    //obteniendo el id del url
+    var id = location.search.match(/id=(\d)*/)[1];
+    //Busca el elemento en el arreglo
+    var evento = eventos.find(function (element) {
+      return element.id == id
+    });
+    //Crea un string que contenga el HTML que describe el detalle del evento
+    var html = "";
+    //Modifica el DOM agregando el html generado dentro del div con id=evento
+    html = `<div class="col">
+            <div class="p-3 border bg-light rounded">
+            <h2>${evento.nombre}</h2>
+            <p class="text-black-50">${evento.fecha} - ${evento.lugar}</p>
+            <p>${evento.descripcion}</p>
+            <p class="text-info">Costo: ${evento.precio}</p>
+            <p class="text-warning">Invitados: ${evento.invitados}</p>
+            </div>
+            </div>`;
 
-  //Guarda el resultado en una variable
-
-  //Busca el elemento en el arreglo
-
-  //Crea un string que contenga el HTML que describe el detalle del evento
-
-  //Modifica el DOM agregando el html generado dentro del div con id=evento
-
+    document.getElementById("evento").innerHTML = html
+  });
 });
